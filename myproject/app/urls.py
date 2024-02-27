65b8fbb15fe016cbcfec0cdd9a9a34629996da3e
aryakp1234 <138677200+aryakp1234@users.noreply.github.com>
2024-02-27 15:51:59 +0530

    Add files via upload

---
+++ myproject/app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .import views
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('ind/',views.home,name='home'),
+    path('home/',views.about,name='about'),
+    path('abt/',views.contact,name='contact'),
+
+
+]
