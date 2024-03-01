adf6708b17ff460f930c8fdb15cf0849a0de778b
aryakp <138677200+aryakp1234@users.noreply.github.com>
2024-03-01 17:22:38 +0530

    Add files via upload

--- project1/proapp/admin.py
+++ project1/proapp/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Signin
+from .models import *
 
 # Register your models here.
+class Signin_dispay(admin.ModelAdmin):
+    list_display=['username','password']
+
 admin.site.register(Signin)
--- project1/proapp/urls.py
+++ project1/proapp/urls.py
@@ -4,5 +4,8 @@ from .import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('page1/',views.home,name='home'),
-    
+    path('list/',views.list,name='list'),
+    path('listform/',views.form1,name='listform'),
+
+   
 ]
